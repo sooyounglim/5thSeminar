def31f0092d7a41d71cfee00ef103ca8d7264bfd
sooyounglim <stellar1198@gmail.com>
2018-06-26 02:12:30 +0900

    commit

---
+++ app/src/main/java/com/gmail/stella1198/myapplication/MainActivity_.kt
@@ -0,0 +1,11 @@
+package com.gmail.stella1198.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_ : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_)
+    }
+}
